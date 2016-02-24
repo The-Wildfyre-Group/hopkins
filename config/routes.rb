6c78314eab6088d304fcb8a08dd5ff3329fd79ea
Cordis Stanfield <cordis.stanfield@gmail.com>
2016-02-23 22:44:42 -0500

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   root 'main#index'
+  resources :surveys do
+    collection do 
+      get :status, as: :status
+      get :services, as: :services
+      get :behavior, as: :behavior
+      get :psychosocial, as: :psychosocial
+      get :closing, as: :closing
+    end
+  end
+  
+  resources :users
+  
+  get "login" => "sessions#new", as: :login
+  get "participate" => "users#new", as: :participate 
+  
+  get "test" => "main#test", as: :test
 end
