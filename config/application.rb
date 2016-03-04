8a9fc2c4afae1ec8fc15e3d197f9871247f63541
Cordis Stanfield <cordis.stanfield@gmail.com>
2016-03-04 14:26:39 -0500

    add config.serve_static_assets = true to application.rb

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Hopkins
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.serve_static_assets = true
     config.time_zone = 'Eastern Time (US & Canada)'
     config.assets.initialize_on_precompile = false
   end
