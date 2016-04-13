3c140b0319b4ee07126a37dda76238c17737a738
Cordis Stanfield <cordis.stanfield@gmail.com>
2016-04-13 12:11:47 -0400

    work on census.rb

--- app/models/census.rb
+++ app/models/census.rb
@@ -8,14 +8,14 @@
     HS_OR_SOME_COLLEGE = [63,84,13,67,93,14,214,289,35,38,52,8].sum
     COLLEGE_OR_MORE = [11,24,3,8,22,3,29,70,9,6,17,3].sum
 
-  
+    
     #[18,24,11,23,23,11,63,84,13,67,93,14,11,24,3,8,22,3,70,85,39,9,9,9,214,289,35,38,52,8,29,70,9,6,17,3]
 
     included do
     end
 
     module ClassMethods
-    
+      
       def calculate_number(region, education, age_range)
         filtered_users = User.where(region: region, education: education)
         if age_range.size == 2
