4c2475795620f4d6f5338cf34522149d69e8cfb8
Cordis Stanfield <cordis.stanfield@gmail.com>
2016-08-25 15:36:21 -0400

    fixed/added full logic for unlocking surveys if previous survey is completed

--- app/helpers/surveys_helper.rb
+++ app/helpers/surveys_helper.rb
@@ -1,2 +1,16 @@
 module SurveysHelper
+  
+  def next_survey_if_previous_completed(user, previous_survey)
+    if user.completed_survey?(previous_survey)
+      return services_surveys_path if previous_survey == "Status"
+      return behavior_surveys_path if previous_survey == "Services"
+      return psychosocial_surveys_path if previous_survey == "Behavior"
+      return closing_surveys_path if previous_survey == "Psycho Social"
+    else
+      "javascript: void(0)"
+    end
+    
+  end
+  
+  
 end
