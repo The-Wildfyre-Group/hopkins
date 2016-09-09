fa327378856070110b3d08384f003728b77d0042
Dmitri Kiselevich <kis.dmitri.gen@gmail.com>
2016-09-10 00:10:37 +0300

    clain card

--- app/services/claim_giftcard.rb
+++ app/services/claim_giftcard.rb
@@ -8,15 +8,12 @@ class ClaimGiftcard
   private
   
   def self.first_available_giftcard
-    codes = AvailableCode.all
+    codes = AvailableCode.all - ClaimedGiftcard.all.pluck(:code)
     i = 0
     while ClaimedGiftcard.exists?(code: codes[i])
       i += 1
     end
     codes[i]
   end
-  
- 
-  
   
 end
