c9d06219ee7046ec84297b1936966944def65a11
Nikita Gnotov <nikita@gnotov.com>
2016-09-14 06:27:16 +0300

    Bio pages. Closes #73

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -19,6 +19,12 @@ class MainController < ApplicationController
   def share_your_story
   end
 
+  def dr_laveist
+  end
+
+  def dr_thorpe
+  end
+
   def test
     @data = HTTParty.get('https://api.typeform.com/v0/form/T6ExzV?key=893f6cea38785bbc15d71f06bcd07bf5e15653dd')
     @services = HTTParty.get('https://api.typeform.com/v0/form/F2ptjY?key=893f6cea38785bbc15d71f06bcd07bf5e15653dd')
