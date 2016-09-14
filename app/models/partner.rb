39c81fedd3b4b245da0eb48250c67fb16e64bad8
Cordis Stanfield <cordis.stanfield@gmail.com>
2016-09-13 22:00:07 -0400

    add back count for partner leaderboard. fixes #156

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,3 +1,14 @@
 class Partner < ActiveRecord::Base
   mount_uploader :logo, ImageUploader
+  has_and_belongs_to_many :users
+  
+  def user_count
+    users.count
+  end
+  
+  def self.ordered_by_participants
+    all.sort_by { |partner| partner.user_count }
+  end
+  
+  
 end
